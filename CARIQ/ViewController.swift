3a31246472ca5513be8cab1e387a0a696d1b46d5
Diya Bhattarai <101464852+diyabhtt@users.noreply.github.com>
2024-11-16 22:43:48 -0600

    hi

---
+++ CARIQ/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CARIQ
+//
+//  Created by Diya Bhattarai on 11/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hi")
+        print("i hate everything")
+        print("hfejnrf")
+    }
+
+
+}
+
---
+++ CARIQUITests/CARIQUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CARIQUITestsLaunchTests.swift
+//  CARIQUITests
+//
+//  Created by Diya Bhattarai on 11/16/24.
+//
+
+import XCTest
+
+final class CARIQUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
